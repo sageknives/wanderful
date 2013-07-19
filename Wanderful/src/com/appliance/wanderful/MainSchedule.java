9127e1c5efd7e8917fb1beff5ddba9afe32bfe7a
sageknives <sageknives@gmail.com>
2013-07-18 20:46:16 -0700

    commiting to roll back

--- Wanderful/src/com/appliance/wanderful/MainSchedule.java
+++ Wanderful/src/com/appliance/wanderful/MainSchedule.java
@@ -35,6 +35,7 @@ public class MainSchedule extends BaseActivity implements TabListener {
 		setContentView(R.layout.activity_main_schedule);
 
 		// gets all buttons and sets them to nav click listeners
+<<<<<<< HEAD
 		createNav(MainSchedule.this,this.findViewById(R.layout.activity_main_schedule));
 		if(isNetworkAvailable())
 		{
@@ -43,6 +44,9 @@ public class MainSchedule extends BaseActivity implements TabListener {
 			oclient.execute(ourl);
 			System.out.print("network is available!");
 		}
+=======
+		createNav(MainSchedule.this,this.findViewById(R.layout.activity_hash_feed));
+>>>>>>> parent of 37b2a3a... added images to the bottom nav bar
 		
 	}
 
