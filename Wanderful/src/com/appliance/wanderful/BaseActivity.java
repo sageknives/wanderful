61199582be5a448f77f1dee12c40b6a55e93c925
sageknives <sageknives@gmail.com>
2013-07-18 23:43:18 -0700

    Fixing commit

--- Wanderful/src/com/appliance/wanderful/BaseActivity.java
+++ Wanderful/src/com/appliance/wanderful/BaseActivity.java
@@ -1,10 +1,7 @@
 package com.appliance.wanderful;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
@@ -50,11 +47,5 @@ public class BaseActivity extends FragmentActivity{
 			if (mapBtn.getId() == v.getId())
 				startActivity(new Intent(curActivity, Map.class));
 		}
-	}
-	protected boolean isNetworkAvailable() {
-	    ConnectivityManager connectivityManager 
-	          = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-	    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-	    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
 	}
 }
