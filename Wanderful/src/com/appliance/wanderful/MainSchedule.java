b18437b8459fcedfcd9dba8dae2a4970eb3d557c
sageknives <sageknives@gmail.com>
2013-07-18 23:44:33 -0700

    commiting

--- Wanderful/src/com/appliance/wanderful/MainSchedule.java
+++ Wanderful/src/com/appliance/wanderful/MainSchedule.java
@@ -5,9 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.ActionBar.TabListener;
@@ -15,7 +12,6 @@ import android.app.FragmentTransaction;
 import android.support.v4.app.ListFragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
@@ -36,6 +32,7 @@ public class MainSchedule extends BaseActivity {
 <<<<<<< HEAD
 
 		createNav(MainSchedule.this,this.findViewById(R.layout.activity_main_schedule));
+<<<<<<< HEAD
 		if(isNetworkAvailable())
 		{
 			JSONGetClient oclient = new JSONGetClient(this, ol);
@@ -45,8 +42,41 @@ public class MainSchedule extends BaseActivity {
 		}
 		createNav(MainSchedule.this,this.findViewById(R.layout.activity_main_schedule));
 
+=======
+>>>>>>> parent of 1943fbf... Changed launcher Icon, made the lists dynamic with Json, Made a search
 		
-	}
+		// Set up the action bar.
+				final ActionBar actionBar = getActionBar();
+				actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+				// Create the adapter that will return a fragment for each of the three days
+				
+				mSectionsPagerAdapter = new SectionsPagerAdapter(
+						getSupportFragmentManager());
+
+				// Set up the ViewPager with the sections adapter.
+				mViewPager = (ViewPager) findViewById(R.id.pager);
+				mViewPager.setAdapter(mSectionsPagerAdapter);
+
+				// When swiping between different sections, select the corresponding
+		
+				mViewPager
+						.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+							@Override
+							public void onPageSelected(int position) {
+								actionBar.setSelectedNavigationItem(position);
+							}
+						});
+
+				// For each day of the schedule, add a tab to the action bar.
+				for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+					// Create a tab with text corresponding to the page title defined by
+					
+					actionBar.addTab(actionBar.newTab()
+							.setText(mSectionsPagerAdapter.getPageTitle(i))
+							.setTabListener(this));
+				}
+			}
 
 	
 =======
@@ -65,12 +95,10 @@ public class MainSchedule extends BaseActivity {
 	
 
 	public class SectionsPagerAdapter extends FragmentPagerAdapter {
-		int numTabs;
-		JSONObject jsonObject;
-		public SectionsPagerAdapter(android.support.v4.app.FragmentManager fm, JSONObject jsonObject) {
+		final int NUM_TAB = 3;
+
+		public SectionsPagerAdapter(android.support.v4.app.FragmentManager fm) {
 			super(fm);
-			this.numTabs = jsonObject.length() + 1;
-			this.jsonObject = jsonObject;
 		}
 
 
@@ -79,16 +107,11 @@ public class MainSchedule extends BaseActivity {
 			// getItem is called to instantiate the fragment for the given page.
 			
 
-			for(int i = 0; i < jsonObject.length();i ++)
-			{
-				if (position == i) {
-					ListFragment list = DummyListFragment.newInstance(position,jsonObject);
-					return  list;
-				}
-			}
-			ListFragment list = DummyListFragment.newInstance(position,jsonObject);
-			return  list;
-			/*} else if (position == 1) {
+			if (position == 0) {
+
+				ListFragment listone = DummyListFragment.newInstance(position);
+				return  listone;
+			} else if (position == 1) {
 
 				ListFragment listtwo = DummyListFragment.newInstance(position);
 				return  listtwo;
@@ -96,7 +119,7 @@ public class MainSchedule extends BaseActivity {
 
 				ListFragment listthree = DummyListFragment.newInstance(position);
 				return  listthree;
-			}*/
+			}
 		}
 			
 
@@ -104,29 +127,21 @@ public class MainSchedule extends BaseActivity {
 		@Override
 		public int getCount() {
 			// Show 3 total pages.
-			return numTabs;
+			return NUM_TAB;
 		}
 
 		@Override
 		public CharSequence getPageTitle(int position) {
 			Locale l = Locale.getDefault();
-			for(int i = 0; i < numTabs -1;i++)
-			{
-				if(position == i) return (getResources().getStringArray(R.array.day_names)[i]).toUpperCase(l);
-				//if(position == numTabs) 
-			}
-			return  "Search".toUpperCase(l);
-				
-			/*switch (position) {
+			switch (position) {
 			case 0:
 				return getString(R.string.Friday).toUpperCase(l);
 			case 1:
 				return getString(R.string.Saturday).toUpperCase(l);
 			case 2:
 				return getString(R.string.Sunday).toUpperCase(l);
-			default:
-				return getString(R.string.search).toUpperCase(l);
-			}*/
+			}
+			return null; 
 		}
 
 		
@@ -158,19 +173,8 @@ public class MainSchedule extends BaseActivity {
 	}
 
 	
-	@SuppressLint("ValidFragment")
 	public static  class DummyListFragment extends ListFragment {
 		View v;
-		JSONObject jsonObject;
-		int num = 0;
-		@SuppressLint("ValidFragment")
-		public DummyListFragment(JSONObject jsonObject,int num) {
-			// TODO Auto-generated constructor stub
-			this.jsonObject = jsonObject;
-			this.num = num;
-			System.out.print(num);
-		}
-
 		/**
 		 * static DummyListFragment newInstance(int num) { DummyListFragment
 		 * f = new DummyListFragment();
@@ -181,14 +185,12 @@ public class MainSchedule extends BaseActivity {
 		 * return f; }
 		 **/
 		
-		@SuppressLint("ValidFragment")
-		static DummyListFragment newInstance(int num,JSONObject jsonObject) {
-			DummyListFragment list = new DummyListFragment(jsonObject,num);
-			
+		static DummyListFragment newInstance(int num) {
+			DummyListFragment list = new DummyListFragment();
+
 	        Bundle args = new Bundle();
 	        args.putInt("num", num);
-	        
-	        
+
 	        list.setArguments(args);
 
 	        return list;
@@ -218,34 +220,25 @@ public class MainSchedule extends BaseActivity {
 		public void onActivityCreated(Bundle savedInstanceState) {
 			super.onActivityCreated(savedInstanceState);
 			
+			List<EventItem> eventItems = new ArrayList<EventItem>();
+
 			String name = null;
 			String time = null;
-			ListAdapter adapter = null;
+
 			// populate the day/time of the list with static data that I
 			// created
 			// in string value folder
-			
-            	// create an array list of eventItem so that we can put as many
-    			// shows/events occurring
-            	ArrayList<EventItem> eventItems = new ArrayList<EventItem>();
-            	try 
-            	{
-		        	for (int j = 0; j < jsonObject.getJSONArray("Day" + (num+1)).length(); j++) 
-		        	{
-						name = jsonObject.getJSONArray("Day" + (num+1)).getJSONObject(j).getString("SubEventName");
-		            	//System.out.println("name test " + i + ":" + name);
-		            	time = jsonObject.getJSONArray("Day" + (num+1)).getJSONObject(j).getString("SubEventStartTime");
-		            	//System.out.println("Time test " + i + ":" + time);
-		            	EventItem tempList = (EventItem) new EventItem(name, time);
-						eventItems.add(tempList);
-		        	}
-            	} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-            	 adapter = new ListAdapter(getActivity(),R.layout.activity_listview_layout,eventItems );
-    			
-            
+			for (int i = 0; i < getResources()
+					.getStringArray(R.array.names).length; i++) {
+
+				name = (getResources().getStringArray(R.array.names)[i]);
+				time = (getResources().getStringArray(R.array.time)[i]);
+				// create a temp list to hold the info and then add to our
+				// ultimate list that has all the events of the expand list
+				EventItem tempList = new EventItem(name, time);
+				eventItems.add(tempList);
+			}
+	         ListAdapter adapter = new ListAdapter(getActivity(),R.layout.activity_listview_layout,eventItems );
 			setListAdapter(adapter);
 
 		}
@@ -255,6 +248,7 @@ public class MainSchedule extends BaseActivity {
 			Log.i("FragmentList", "Item clicked: " + id);
 		}
 	}
+<<<<<<< HEAD
 	JSONClientListener ol = new JSONClientListener() {
 
 		public void onRemoteCallComplete(JSONObject jsonObjectFromNet)throws JSONException 
@@ -296,4 +290,6 @@ public class MainSchedule extends BaseActivity {
 	};
 =======
 >>>>>>> parent of a5c9a58... Merge branch 'master' of https://github.com/sageknives/wanderful
+=======
+>>>>>>> parent of 1943fbf... Changed launcher Icon, made the lists dynamic with Json, Made a search
 }
