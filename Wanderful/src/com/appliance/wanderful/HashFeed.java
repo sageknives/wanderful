4909ffd5bdf7aa3be985193df4eb40df8cbb5e19
sageknives <sageknives@gmail.com>
2013-07-22 17:39:54 -0700

    added saving performances and events to baseactivity. added classes to hold performance and event information and stored them in a static arraylist in baseactivity. Made the map page download a map. Save it to a static Bitmap in baseactivity. Added launcher icon. added a background image for start screen

--- Wanderful/src/com/appliance/wanderful/HashFeed.java
+++ Wanderful/src/com/appliance/wanderful/HashFeed.java
@@ -1,15 +1,17 @@
 package com.appliance.wanderful;
 
-import java.util.ArrayList;
-
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class HashFeed extends BaseActivity {
 	Button eventbtn;
@@ -22,60 +24,37 @@ public class HashFeed extends BaseActivity {
 		// gets all buttons and sets them to nav click listeners
 		createNav(HashFeed.this,this.findViewById(R.layout.activity_hash_feed));
 		
-		//sends a get request for festival information
+		//checks to see if base activity has a list of performances and if it is the performances for this event
+		if(performances.size() == 0)
+		{
+			requestWebInfo();
+			System.out.println("size test: ");
+		}
+		else if(performances.get(1).getEventID() != currentEventID)
+		{
+			requestWebInfo();
+			System.out.println("event ID match test: ");
+		}
+		else{
+			setupPage();
+		}
+	}
+	public void requestWebInfo()
+	{
 		JSONGetClient client = new JSONGetClient(this, jsonGet);
 		String ourl = "http://54.218.117.137/scoutservices/jsonobjectcommand.php?user=scoutreader&pass=readscout";
 		client.execute(ourl);
 	}
-	public void saveEventInfo(JSONObject jsonObject)
-	{
-		for(int i = 1; i<jsonObject.length() + 1;i++)
-		{
-			try 
-        	{
-	        	for (int j = 0; j < 1; j++) 
-	        	{
-	        		int eventID = 0;
-					String name = jsonObject.getJSONArray("Day" + (i)).getJSONObject(j).getString("EventName");
-					String startDate = jsonObject.getJSONArray("Day" + (i)).getJSONObject(j).getString("EventStartDate");
-	            	String length = jsonObject.getJSONArray("Day" + (i)).getJSONObject(j).getString("EventNumDays");
-	            	String lastUpdated = "2013-10-23";
-	            	String logo = "logo";
-	            	String location = jsonObject.getJSONArray("Day" + (i)).getJSONObject(j).getString("EventLocation");
-	            	String map = "map";
-					events.add(new Event(eventID,name,startDate,length,lastUpdated,logo,location,map));
-	        	}
-        	} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		}
-	}
-	public void savePerformancesInfo(JSONObject jsonObject,int eventID)
-	{
-		for(int i = 1; i<jsonObject.length() + 1;i++)
-		{
-			try 
-        	{
-	        	for (int j = 0; j < jsonObject.getJSONArray("Day" + (i)).length(); j++) 
-	        	{
-					String name = jsonObject.getJSONArray("Day" + (i)).getJSONObject(j).getString("SubEventName");
-	            	String time = jsonObject.getJSONArray("Day" + (i)).getJSONObject(j).getString("SubEventStartTime");
-	            	String stage = jsonObject.getJSONArray("Day" + (i)).getJSONObject(j).getString("SubEventLocation");
-	            	String day = jsonObject.getJSONArray("Day" + (i)).getJSONObject(j).getString("SubEventDay");
-	            	String description = jsonObject.getJSONArray("Day" + (i)).getJSONObject(j).getString("SubEventDescription");
-	            	String image = "image";
-	            	String media = "media";
-					performances.add(new Performance(eventID,j,name,time,stage,day,description,image,media));
-	        	}
-        	} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		}
-		
+	public void setupPage(){
+		//changes the text view 1 to the first band on the first day
+		TextView header = (TextView)findViewById(R.id.header);
+		header.setText(events.get(0).getEventName());
+        TextView txtView1 = (TextView)findViewById(R.id.text1);
+        txtView1.setText(performances.get(0).getPerformanceArtistName());
+        TextView txtView2 = (TextView)findViewById(R.id.text2);
+        txtView2.setText(performances.get(0).getPerformanceTime());
+		eventbtn = (Button) findViewById(R.id.event_btn);
+		eventbtn.setOnClickListener(new eventClickListeners());
 	}
 	JSONClientListener jsonGet = new JSONClientListener() {
 
@@ -85,15 +64,7 @@ public class HashFeed extends BaseActivity {
 			int eventID=1;
 			savePerformancesInfo(jsonObjectFromNet,eventID);
 			saveEventInfo(jsonObjectFromNet);
-			//changes the text view 1 to the first band on the first day
-			TextView header = (TextView)findViewById(R.id.header);
-			header.setText(events.get(0).getEventName());
-            TextView txtView1 = (TextView)findViewById(R.id.text1);
-            txtView1.setText(performances.get(0).getPerformanceArtistName());
-            TextView txtView2 = (TextView)findViewById(R.id.text2);
-            txtView2.setText(performances.get(0).getPerformanceTime());
-    		eventbtn = (Button) findViewById(R.id.event_btn);
-    		eventbtn.setOnClickListener(new eventClickListeners());
+			setupPage();
         }
 	};
 	private class eventClickListeners implements View.OnClickListener {
--- Wanderful/src/com/appliance/wanderful/SearchEvent.java
+++ Wanderful/src/com/appliance/wanderful/SearchEvent.java
@@ -1,9 +1,15 @@
 package com.appliance.wanderful;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SearchEvent extends BaseActivity {
+	Button enterBtn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -11,9 +17,20 @@ public class SearchEvent extends BaseActivity {
 		setContentView(R.layout.activity_search_event);
 
 		// gets all buttons and sets them to nav click listeners
-		createNav(SearchEvent.this,this.findViewById(R.layout.activity_hash_feed));
+		enterBtn = (Button) findViewById(R.id.enter_btn);
+		enterBtn.setOnClickListener(new enterClickListeners());
 	}
-
+	public class enterClickListeners implements View.OnClickListener {
+		@Override
+		public void onClick(View v) {
+			// Gets the button Id and sends to new activity
+			
+			if (enterBtn.getId() == v.getId())
+				currentEventID = 1;
+				currentMapImage = null;
+				startActivity(new Intent(SearchEvent.this, MainSchedule.class));
+		}
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
